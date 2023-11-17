02c79ad6a54123056eac3749d33d7d2f732d4ef6
arthurj124 <58998651+arthurj124@users.noreply.github.com>
2023-11-17 18:17:23 -0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,14 +1,18 @@
  // URL da API
+function getApiGitHub(){
+    fetch ('http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL').then((res) => res.json()).then((data)=> dolar = data['USDBRL']['high'] );
 
+}
 
 
 
 let dolar = 0;
-fetch ('http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL').then((res) => res.json()).then((data)=> dolar = data['USDBRL']['high'] );
-
+getApiGitHub()
 
 
 function calcular(){
+    getApiGitHub()
+
     alert(dolar);
     
 
